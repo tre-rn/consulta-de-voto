547e192013e21160707bb148cb62db0a603f57dd
tre-rn <117474565+tre-rn@users.noreply.github.com>
2022-11-04 14:35:53 -0300

    add all files

---
+++ script.js
@@ -0,0 +1,4 @@
+const eleitor = prompt("Tribunal Regional Eleitoral do Rio Grande do Norte - Consulta de Voto\n\nInforme seu nome e sobrenome: ")
+prompt("Informe os 3 primeiros dígitos do seu CPF:")
+
+document.querySelector("#eleitor").innerHTML = eleitor
